01a483eaf257916c5ccafd9602bc873122b9ae35
gerinka <dodstes@gmail.com>
2016-04-06 07:42:37 +0300

    init commit nice way

---
+++ MasterThesisCreator/Controllers/ThesisSetupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterThesisCreator.Controllers
+{
+    public class ThesisSetupController : Controller
+    {
+        // GET: ThesisSetup
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MasterThesisCreator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasterThesisCreator.Startup))]
+namespace MasterThesisCreator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
