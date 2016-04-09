639267679a969b3a4b721273cb801f4de822de51
gerinka <dodstes@gmail.com>
2016-04-09 10:04:48 +0300

    trying to use ready to go template

--- MasterThesisCreator/App_Start/BundleConfig.cs
+++ MasterThesisCreator/App_Start/BundleConfig.cs
@@ -22,19 +22,24 @@ namespace MasterThesisCreator
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/bower_components/bootstrap/bootstrap/dist/js.js",
-                      "~/bower_components/respond/dest/respond.src.js"));
+                      "~/bower_components/respond/dest/respond.src.js",
+                      "~/bower_components/bootstrap-wysihtml5/dist/bootstrap-wysihtml5-0.0.2.js",
+                      "~/bower_components/bootstrap-select/dist/js/bootstrap-select.js",
+                      "~/bower_components/tags/dist/tags.js",
+                      "~/bower_components/twitter-bootstrap-wizard/jquery.bootstrap.wizard.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap-datepicker").Include(
+        "~/bower_components/moment/moment.js",
+        "~/bower_components/moment/locale/bg.js",
         "~/bower_components/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-wysihtml5").Include(
-        "~/bower_components/bootstrap-wysihtml5/dist/bootstrap-wysihtml5-0.0.2.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/bower_components/bootstrap/dist/css/bootstrap.css",
-                      "~/Content/site.css",
+                      "~/bower_components/bootstrap/dist/css/bootstrap.css", 
                       "~/bower_components/eonasdan-bootstrap-datetimepicker/src/_bootstrap-datetimepicker.less",
-                      "~/bower_components/bootstrap-wysihtml5/dist/bootstrap-wysihtml5-0.0.2.css"));
+                      "~/bower_components/bootstrap-wysihtml5/dist/bootstrap-wysihtml5-0.0.2.css",
+                      "~/bower_components/bootstrap-select/dist/css/bootstrap-select.css",
+                      "~/Content/site.css",
+                      "~/Content/Common/*.css"));
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = false;
