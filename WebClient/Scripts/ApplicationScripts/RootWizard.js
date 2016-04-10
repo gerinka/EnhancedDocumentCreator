e1484300a1714c350c9aabaa4e306e1a5a42fa5b
gerinka <dodstes@gmail.com>
2016-04-10 11:12:09 +0300

    showing the proper tab menu

--- WebClient/Scripts/ApplicationScripts/RootWizard.js
+++ WebClient/Scripts/ApplicationScripts/RootWizard.js
@@ -5,10 +5,18 @@
             var $current = index + 1;
             var $percent = ($current / $total) * 100;
             $('#rootwizard .progress-bar').css({ width: $percent + '%' });
+            $(".nav-pills li").addClass("invisible");
+            $(".nav-pills li.active").removeClass("invisible");
         }
     });
     $('#deadline').datetimepicker({
         locale: 'bg',
         format: 'DD MMMM YYYY'
+    });
+
+    $('.dropdown-menu a').on('click', function() {
+        if (!$(this).parent().hasClass("disabled")) {
+            $('.dropdown-toggle').html($(this).html() + '<span class="caret"></span>');
+        }
     });
 });
