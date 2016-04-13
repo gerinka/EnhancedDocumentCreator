7086a7c1097219e38686c2a46b581c35a6e4d700
gerinka <dodstes@gmail.com>
2016-04-13 08:11:22 +0300

    updating a little the logic about the checkboxes

--- WebClient/Scripts/ApplicationScripts/CheckboxesLists.js
+++ WebClient/Scripts/ApplicationScripts/CheckboxesLists.js
@@ -1,9 +1,21 @@
-﻿$('.collapse').collapse();
-$('#topic1 a input[type=checkbox]').on('click', function (e) {
-    e.stopPropagation();
-})
-$('#collapseOne').on('show.bs.collapse', function (e) {
-    if (!$('#topic1 a input[type=checkbox]').is(':checked')) {
-        return false;
-    }
+﻿$(document).ready(function() {
+    $('.collapse').collapse('hide').on('hide.bs.collapse', function() {
+        console.log(this);
+    });
+
+    $('#topic1 a input[type=checkbox]').on('click', function(e) {
+        e.stopPropagation();
+        if (!this.checked) {
+            $('#collapseOne').removeClass("collapse");
+            $('#collapseOne').addClass("in");
+        }
+    });
+
+    $('#collapseOne').on('show.bs.collapse', function(e) {
+        if (!$('#topic1 a input[type=checkbox]').is(':checked')) {
+            return false;
+        }
+    });
+
+    $(':checkbox').prop('checked', true);
 });
