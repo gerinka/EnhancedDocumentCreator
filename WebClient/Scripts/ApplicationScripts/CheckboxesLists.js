9b37f3bb50bcc3e61654cf880c520f1a10a06477
gerinka <dodstes@gmail.com>
2016-04-16 17:57:27 +0300

    trying to make the checkboxes working with collapse part1

--- WebClient/Scripts/ApplicationScripts/CheckboxesLists.js
+++ WebClient/Scripts/ApplicationScripts/CheckboxesLists.js
@@ -1,21 +1,31 @@
 ﻿$(document).ready(function() {
-    $('#collapseOne').collapse('hide').on('hide.bs.collapse', function () {
+    $('#collapseTopic1').collapse('hide').on('hide.bs.collapse', function () {
         console.log(this);
     });
 
-    $('#topic1 a input[type=checkbox]').on('click', function(e) {
+    $('div.active input[type=checkbox].main-topic').on('click', function (e) {
         e.stopPropagation();
         if (!this.checked) {
-            $('#collapseOne').removeClass("collapse");
-            $('#collapseOne').addClass("in");
+            $('#collapseTopic1').removeClass("collapse");
+        } else {
+            $('#collapseTopic1').addClass("collapse");
         }
     });
 
-    $('#collapseOne').on('show.bs.collapse', function(e) {
-        if (!$('#topic1 a input[type=checkbox]').is(':checked')) {
+    $('#collapseTopic1').on('show.bs.collapse', function (e) {
+        if (!$('div.active input[type=checkbox].main-topic').is(':checked')) {
             return false;
         }
     });
 
     $(':checkbox').prop('checked', true);
+    $(".main-topic").change(function () {
+        if (this.checked) {
+            console.log("fsdfs");
+        }
+    });
 });
+function changeState(el) {
+    if (el.readOnly) el.checked = el.readOnly = false;
+    else if (!el.checked) el.readOnly = el.indeterminate = true;
+}
