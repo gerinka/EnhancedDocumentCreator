7f339c48ceac80c7d986fbe406820e4a1d7da34e
gerinka <dodstes@gmail.com>
2016-04-30 17:43:13 +0300

    testing DocumentCreation

---
+++ DocumentCreation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mtc.Domain;
+
+namespace DocumentCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentGenerator.GenerateDocument("temp");
+        }
+    }
+}
---
+++ Mtc.Domain/DocumentGenerator.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using Mtc.Domain.Interfaces;
+using TemplateEngine.Docx;
+
+namespace Mtc.Domain
+{
+    public class DocumentGenerator : IDocumentGenerator
+    {
+        public static void GenerateDocument(string title)
+        {				
+            File.Delete("OutputDocument.docx");
+            File.Copy("InputTemplate.docx", "OutputDocument.docx");
+
+		
+	        var valuesToFill = new Content(
+		        // Add field.
+		        new FieldContent("Report date", DateTime.Now.ToString()),
+                new FieldContent("DocumentTitle", title),
+		        // Add table.
+		        new TableContent("Team Members Table")
+			        .AddRow(
+				        new FieldContent("Name", "Eric"),
+				        new FieldContent("Role", "Program Manager"))
+			        .AddRow(
+				        new FieldContent("Name", "Bob"),
+				        new FieldContent("Role", "Developer")),
+
+				// Add field inside table that not to propagate.
+		        new FieldContent("Count", "2"),
+
+				// Add list.	
+				new ListContent("Team Members List")
+					.AddItem(
+						new FieldContent("Name", "Eric"), 
+						new FieldContent("Role", "Program Manager"))
+					.AddItem(
+						new FieldContent("Name", "Bob"),
+						new FieldContent("Role", "Developer")),
+
+				// Add nested list.	
+				new ListContent("Team Members Nested List")
+					.AddItem(new ListItemContent("Role", "Program Manager")
+						.AddNestedItem(new FieldContent("Name", "Eric"))
+						.AddNestedItem(new FieldContent("Name", "Ann")))
+					.AddItem(new ListItemContent("Role", "Developer")
+						.AddNestedItem(new FieldContent("Name", "Bob"))
+						.AddNestedItem(new FieldContent("Name", "Richard"))),
+
+				// Add list inside table.	
+				new TableContent("Projects Table")
+					.AddRow(
+						new FieldContent("Name", "Eric"), 
+						new FieldContent("Role", "Program Manager"), 
+						new ListContent("Projects")
+							.AddItem(new FieldContent("Project", "Project one"))
+							.AddItem(new FieldContent("Project", "Project two")))
+					.AddRow(
+						new FieldContent("Name", "Bob"),
+						new FieldContent("Role", "Developer"),
+						new ListContent("Projects")
+							.AddItem(new FieldContent("Project", "Project one"))
+							.AddItem(new FieldContent("Project", "Project three"))),
+		      
+				// Add table inside list.	
+				new ListContent("Projects List")
+					.AddItem(new ListItemContent("Project", "Project one")
+						.AddTable(TableContent.Create("Team members")
+							.AddRow(
+								new FieldContent("Name", "Eric"), 
+								new FieldContent("Role", "Program Manager"))
+							.AddRow(
+								new FieldContent("Name", "Bob"), 
+								new FieldContent("Role", "Developer"))))
+					.AddItem(new ListItemContent("Project", "Project two")
+						.AddTable(TableContent.Create("Team members")
+							.AddRow(
+								new FieldContent("Name", "Eric"),
+								new FieldContent("Role", "Program Manager"))))
+					.AddItem(new ListItemContent("Project", "Project three")
+						.AddTable(TableContent.Create("Team members")
+							.AddRow(
+								new FieldContent("Name", "Bob"),
+								new FieldContent("Role", "Developer")))),
+
+
+				// Add table with several blocks.	
+				new TableContent("Team Members Statistics")
+					.AddRow(
+						new FieldContent("Name", "Eric"),
+						new FieldContent("Role", "Program Manager"))
+					.AddRow(
+					    new FieldContent("Name", "Richard"),
+						new FieldContent("Role", "Program Manager"))
+					.AddRow(
+						new FieldContent("Name", "Bob"),
+						new FieldContent("Role", "Developer")),
+
+					new TableContent("Team Members Statistics")
+					.AddRow(
+						new FieldContent("Statistics Role", "Program Manager"),
+						new FieldContent("Statistics Role Count", "2"))						
+					.AddRow(
+						new FieldContent("Statistics Role", "Developer"),
+						new FieldContent("Statistics Role Count", "1")),
+						
+				// Add table with merged rows
+				new TableContent("Team members info")
+					.AddRow(
+						new FieldContent("Name", "Eric"),
+						new FieldContent("Role", "Program Manager"),
+						new FieldContent("Age", "37"),
+						new FieldContent("Gender", "Male"))
+					.AddRow(
+						new FieldContent("Name", "Bob"),
+						new FieldContent("Role", "Developer"),
+						new FieldContent("Age", "33"),
+						new FieldContent("Gender", "Male"))
+					.AddRow(
+						new FieldContent("Name", "Ann"),
+						new FieldContent("Role", "Developer"),
+						new FieldContent("Age", "34"),
+						new FieldContent("Gender", "Female")),
+						
+				// Add table with merged columns
+				new TableContent("Team members projects")
+					.AddRow(
+						new FieldContent("Name", "Eric"),
+						new FieldContent("Role", "Program Manager"),
+						new FieldContent("Age", "37"),
+						new FieldContent("Projects", "Project one, Project two"))
+					.AddRow(
+						new FieldContent("Name", "Bob"),
+						new FieldContent("Role", "Developer"),
+						new FieldContent("Age", "33"),
+						new FieldContent("Projects", "Project one"))
+					.AddRow(
+						new FieldContent("Name", "Ann"),
+						new FieldContent("Role", "Developer"),
+						new FieldContent("Age", "34"),
+						new FieldContent("Projects", "Project two")));
+
+            using(var outputDocument = new TemplateProcessor("OutputDocument.docx")
+				.SetRemoveContentControls(true))
+            {
+                outputDocument.FillContent(valuesToFill);
+                outputDocument.SaveChanges();
+            }
+        }
+    }
+}
+
