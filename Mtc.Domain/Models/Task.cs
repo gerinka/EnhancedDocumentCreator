93f0114d13f8414d6e99d55d366e6f3adc1e2a28
gerinka <dodstes@gmail.com>
2016-05-01 11:26:04 +0300

    updating task model

--- Mtc.Domain/Models/Task.cs
+++ Mtc.Domain/Models/Task.cs
@@ -10,6 +10,7 @@ namespace Mtc.Domain.Models
     public class Task
     {
         public long Id { get; set; }
+        public string Title { get; set; }
         public Person AssignTo { get; set; }
         public TaskType TaskType { get; set; }
         public TaskState TaskState { get; set; }
