6fb9f33e31ddda0ec05bb2f2b35b7a7f406623fd
gerinka <dodstes@gmail.com>
2016-05-07 15:31:52 +0300

    moving services to domain

--- Mtc.Infrastructure.Services/Interfaces/IPersonService.cs
+++ Mtc.Infrastructure.Services/Interfaces/IPersonService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mtc.Infrastructure.Services.Interfaces
+﻿namespace Mtc.Domain.Services.Interfaces
 {
     interface IPersonService
     {
--- Mtc.Infrastructure.Services/Properties/AssemblyInfo.cs
+++ Mtc.Infrastructure.Services/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mtc.Infrastructure.Services")]
+[assembly: AssemblyTitle("Mtc.Domain.Services")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mtc.Infrastructure.Services")]
+[assembly: AssemblyProduct("Mtc.Domain.Services")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
