90e8d856102998c77dced6226f9bdea856076f9b
gerinka <dodstes@gmail.com>
2016-05-08 10:22:40 +0300

    update some packages

--- Mtc.Infrastructure.Services/Interfaces/IPersonService.cs
+++ Mtc.Infrastructure.Services/Interfaces/IPersonService.cs
@@ -1,6 +1,13 @@
-﻿namespace Mtc.Domain.Services.Interfaces
+﻿using Mtc.Domain.Models;
+
+namespace Mtc.Domain.Services.Interfaces
 {
     interface IPersonService
     {
+        Person GetPersonById();
+        Person GetPersonByName(string name);
+        Person CreatePerson(Person person);
+        Person UpdatePerson(Person person);
+        Person DeletePerson(Person person);
     }
 }
---
+++ Mtc.Infrastructure.Services/PersonService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mtc.Domain.Models;
+using Mtc.Domain.Services.Interfaces;
+
+namespace Mtc.Domain.Services
+{
+    class PersonService : IPersonService
+    {
+        public Person GetPersonById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetPersonByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person CreatePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person UpdatePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person DeletePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
