5879f1aaefb7c6676b00de309c610fd0942a31a8
gerinka <dodstes@gmail.com>
2016-05-10 11:07:02 +0300

    staging before creating of new edml..

--- Mtc.Domain.Services.Tests/PersonServiceUnitTest.cs
+++ Mtc.Domain.Services.Tests/PersonServiceUnitTest.cs
@@ -1,18 +1,34 @@
-﻿using System;
-using System.Data.Entity;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Transactions;
 using Moq;
-using Mtc.Infrastructure.DataAccess.Repositories;
+using Mtc.Infrastructure.DataAccess.Interfaces;
 using MtcModel;
 using NSubstitute;
+using NUnit.Framework;
 
 namespace Mtc.Domain.Services.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class PersonServiceUnitTest
     {
-        [TestMethod]
-        public void GetPerson()
+        private TransactionScope scope;
+
+        [SetUp]
+        public void SetUp()
+        {
+            scope = new TransactionScope();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            scope.Dispose();
+        }
+
+
+        [Test]
+        public void YourTest()
         {
             var user = new USER()
             {
@@ -20,10 +36,14 @@ namespace Mtc.Domain.Services.Tests
                 FirstName = "Ivan",
                 FamilyName = "Marinov"
             };
+            var mockSet = new Mock<ObjectSet<USER>>();
 
+            var mockContext = new Mock<MtcEntities>();
+            mockContext.Setup(m => m.USERs).Returns(mockSet.Object);
+            var userrepo2 = new Mock<IUserRepository>(mockContext.Object);
             var dbContext = new Mock<MtcEntities>();
-            dbContext.Setup(db => db.USERs);
-            var userRepo = new Mock<UserRepository>(dbContext.Object);
+         
+            var userRepo = new Mock<IUserRepository>(dbContext.Object);
             userRepo.Setup(u => u.GetById(Arg.Any<long>())).Returns(user);
             var personService = new PersonService(userRepo.Object);
             personService.GetPersonById(5);
