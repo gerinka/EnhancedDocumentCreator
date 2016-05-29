0476c1172087c5e76fdddf1eb44de88857cbb51a
gerinka <dodstes@gmail.com>
2016-05-29 11:06:20 +0300

    adjusting maxcycle calculation

--- Mtc.Domain.Common/DeadlineCalculator.cs
+++ Mtc.Domain.Common/DeadlineCalculator.cs
@@ -9,14 +9,14 @@ namespace Mtc.Domain.Common
     public static class DeadlineCalculator
     {
 
-        public static DateTime CalculateDeadline(DateTime documentDeadline, int previousTasks, int totalContentCount, int wave)
+        public static DateTime CalculateDeadline(DateTime documentDeadline, int previousTasks, int totalContentCount, int cycle)
         {
             var timePerTask = (documentDeadline - DateTime.UtcNow).TotalDays / totalContentCount;
             return DateTime.UtcNow.AddDays((previousTasks + 1) * timePerTask);
         }
 
         public static int CalculateMaxCycles(DateTime documentDeadline, int totalContentCount){
-            return (int)Math.Floor((documentDeadline - DateTime.UtcNow).TotalDays / 30) + 1;
+            return (int)Math.Floor((documentDeadline - DateTime.UtcNow).TotalDays /(2* totalContentCount)) + 1;
             
         }
     }
