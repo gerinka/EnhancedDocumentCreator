bd7bfb2bb3861c40d13481127283d807c528b60f
gerinka <dodstes@gmail.com>
2016-06-22 11:05:46 +0300

    Adding simple api

--- Edc.Api/App_Start/WebApiConfig.cs
+++ Edc.Api/App_Start/WebApiConfig.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using Edc.Domain.Common;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -16,7 +18,8 @@ namespace Edc.Api
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+            config.Formatters.Add(new BrowserJsonFormatter());
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
---
+++ Edc.Api/Controllers/DocumentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Edc.Domain.Models;
+using Edc.Domain.Services.Interfaces;
+
+namespace Edc.Api.Controllers
+{
+    [Authorize]
+    public class DocumentController : ApiController
+    {
+        private readonly IDocumentService _documentService;
+
+        public DocumentController(IDocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+
+        // GET api/document
+        public IEnumerable<string> Get()
+        {
+            return _documentService.GetAll().Select(d => d.Title);
+        }
+
+        // GET api/document/5
+        public IHttpActionResult Get(int id)
+        {
+            Document document = _documentService.GetById(id);
+            return Ok(document);
+        }
+
+        // POST api/document
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/document/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/document/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
