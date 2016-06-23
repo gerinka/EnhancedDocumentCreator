cd89452608d66bb2727c291c5f25b5481013a674
gerinka <dodstes@gmail.com>
2016-06-23 08:18:55 +0300

    Fixing issue with repeated keywords in the document export

--- Edc.Domain/Models/Keyword.cs
+++ Edc.Domain/Models/Keyword.cs
@@ -4,5 +4,40 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            var k = obj as Keyword;
+            if ((System.Object)k == null)
+            {
+                return false;
+            }
+
+            return (Id == k.Id) && (Name == k.Name);
+        }
+
+        public bool Equals(Keyword k)
+        {
+            // If parameter is null return false:
+            if ((object)k == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return (Id == k.Id) && (Name == k.Name);
+        }
+
+
+        public override int GetHashCode()
+        {
+            if (Name == null) return 0;
+            return Name.GetHashCode();
+        }
     }
 }
